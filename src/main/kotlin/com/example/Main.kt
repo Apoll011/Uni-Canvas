410ee5cc5564b88212a06fe0cfacc196f05480c3
tiago <tiagorobotik@gmai.com>
2025-10-23 08:41:42 +0100

    Refactored tic-tac-toe game: improved code readability, replaced `MutableList` with `List`, introduced constants for board configuration, and updated naming conventions for consistency.

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -1,15 +1,20 @@
 package com.example
 
 import pt.isel.canvas.*
+import kotlin.collections.mapIndexed
+
+const val BOARD_SIZE = 500;
+const val COLS_PER_ROW = 3;
+const val CELL_SIZE = BOARD_SIZE / COLS_PER_ROW;
 
 fun grid(canvas: Canvas) {
-    canvas.drawLine(canvas.width/3, 0, canvas.width/3, canvas.height)
-    canvas.drawLine(canvas.width*2/3, 0, canvas.width*2/3, canvas.height)
-    canvas.drawLine(0, canvas.height/3, canvas.width, canvas.height/3)
-    canvas.drawLine(0, canvas.height*2/3, canvas.width, canvas.height*2/3)
+    canvas.drawLine(CELL_SIZE, 0, CELL_SIZE, canvas.height)
+    canvas.drawLine(CELL_SIZE*2, 0, CELL_SIZE*2, canvas.height)
+    canvas.drawLine(0, CELL_SIZE, canvas.width, CELL_SIZE)
+    canvas.drawLine(0, CELL_SIZE*2, canvas.width, CELL_SIZE*2)
 }
 
-fun get_cell(x: Int, y: Int, canvas: Canvas): Int {
+fun getCell(x: Int, y: Int, canvas: Canvas): Int {
     val w_s = canvas.width/3
     val h_s = canvas.height/3
 
@@ -19,17 +24,11 @@ fun get_cell(x: Int, y: Int, canvas: Canvas): Int {
     return pos_x + pos_y * 3
 }
 
-fun register_game_move(game: MutableList<Char>, indice: Int, player: Char) : MutableList<Char> {
-    if (game[indice] != '_') {
-        println("Not a Valid Move, Select an Empty Cell")
-        return game
-    }
-
-    game[indice] = player
-    return game
+fun registerGameMove(game: List<Char>, indice: Int, player: Char) : List<Char> {
+    return game.mapIndexed { i, existing ->  if (i == indice) player else existing }
 }
 
-fun draw_icon(x: Int, y: Int, player: Char, canvas: Canvas) {
+fun drawIcon(x: Int, y: Int, player: Char, canvas: Canvas) {
     if (player == 'O') {
         canvas.drawCircle( x, y, 50, RED, 10)
     }
@@ -41,7 +40,7 @@ fun draw_icon(x: Int, y: Int, player: Char, canvas: Canvas) {
     }
 }
 
-fun draw(game: MutableList<Char>, canvas: Canvas) {
+fun draw(game: List<Char>, canvas: Canvas) {
     for (y in 0..2) {
         for (x in 0..2) {
             val i = x + y * 3
@@ -49,22 +48,22 @@ fun draw(game: MutableList<Char>, canvas: Canvas) {
             val x = x * (canvas.width/3) + (canvas.width/3/2)
             val y = y * (canvas.height/3) + (canvas.height/3/2)
 
-            draw_icon(x, y, game[i], canvas)
+            drawIcon(x, y, game[i], canvas)
         }
     }
 }
 
-fun draw_game(game: MutableList<Char>, player: Char, canvas: Canvas) {
+fun drawGame(game: List<Char>, player: Char, canvas: Canvas) {
     canvas.erase()
     draw(game, canvas)
     grid(canvas)
 }
 
-fun is_board_full(game: MutableList<Char>): Boolean {
+fun isBoardFull(game: List<Char>): Boolean {
     return !game.contains('_')
 }
 
-fun won(game: MutableList<Char>, player: Char): Boolean {
+fun won(game: List<Char>, player: Char): Boolean {
     return listOf(
         listOf(0,1,2), listOf(3,4,5), listOf(6,7,8),
         listOf(0,3,6), listOf(1,4,7), listOf(2,5,8),
@@ -73,38 +72,52 @@ fun won(game: MutableList<Char>, player: Char): Boolean {
 
 }
 
-fun any_won(game: MutableList<Char>): Boolean {
+fun anyWon(game: List<Char>): Boolean {
     return won(game, 'O') or won(game, 'X')
 }
 
+fun validPosition(game: List<Char>, indice: Int): Boolean {
+    if (game[indice] != '_') {
+        println("Not a Valid Move, Select an Empty Cell")
+        return false
+    }
+
+    return true
+}
+
 fun main() {
-    var game: MutableList<Char> = MutableList(9) {
+    var game: List<Char> = List(9) {
         '_'
     }
     var currentPlayer = 'X'
     onStart {
-        val arena = Canvas(500, 500, WHITE)
+        val arena = Canvas(BOARD_SIZE, BOARD_SIZE, WHITE)
         grid(arena)
 
         arena.onMouseDown { me ->
-            if (any_won(game)) return@onMouseDown
+            val i = getCell(me.x, me.y, arena)
 
-            val i = get_cell(me.x, me.y, arena)
+            if (anyWon(game)) return@onMouseDown
+            if (!validPosition(game, i)) return@onMouseDown
 
-            game = register_game_move(game, i, currentPlayer)
-            draw_game(game, currentPlayer, arena)
+            game = registerGameMove(game, i, currentPlayer)
+            drawGame(game, currentPlayer, arena)
 
-            if (any_won(game)) {
+            if (anyWon(game)) {
                 arena.drawText(arena.width/4, arena.height/2, "$currentPlayer won the game!", BLACK)
             }
+            else if (isBoardFull(game)) {
+                arena.drawText(arena.width/2, arena.height/2, "It's a tie!", BLACK)
+            }
+
 
             currentPlayer = if (currentPlayer == 'X') 'O' else 'X'
         }
 
         arena.onMouseMove { me ->
-            if (!is_board_full(game) and !any_won(game)) {
-                draw_game(game, currentPlayer, arena)
-                draw_icon(me.x, me.y, currentPlayer, arena)
+            if (!isBoardFull(game) and !anyWon(game)) {
+                drawGame(game, currentPlayer, arena)
+                drawIcon(me.x, me.y, currentPlayer, arena)
             }
         }
     }
